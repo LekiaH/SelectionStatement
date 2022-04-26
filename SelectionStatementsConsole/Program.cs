ad81e063f021cf36552b5f6d81266cb08388c2d6
LekiaH <lekia.hood@gmail.com>
2022-04-26 17:04:12 -0400

    initial commit

---
+++ SelectionStatementsConsole/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace SelectionStatementsConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int favNum = 60;
+            Console.WriteLine("Guess my favorite number. Hint: Below 75.");
+            int userGuess = int.Parse(Console.ReadLine());
+
+            if (userGuess < favNum)
+            {
+                Console.WriteLine("Too Low!");
+
+            }
+            else if (userGuess > favNum)
+            {
+                Console.WriteLine("Too High");
+            }
+            else if (userGuess == favNum)
+            {
+                Console.WriteLine("You were correct!"); 
+            }
+            else
+            {
+                Console.WriteLine("nevermind");
+            }
+            
+
+            
+
+            
+        }
+    }
+}
